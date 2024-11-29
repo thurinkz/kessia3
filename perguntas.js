13a27a4e9a12bfbf6f1fa73515bd5aabf2cb066e
Arthur Silva <168222534+thurinkz@users.noreply.github.com>
2024-11-29 16:38:42 -0300

    kessia

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'A Revolução Francesa aconteceu em 1789',
+    'Quando aconteceu a Revolução Francesa?'
+)
+
+criaCartao(
+    'Matemática',
+    'O valor de Pi é aproximadamente 3,14',
+    'Qual o valor de Pi?'
+)
+
+criaCartao(
+    'Química',
+    'A fórmula da água é H2O',
+    'Qual é a fórmula da água?'
+)
+
+criaCartao(
+    'Astronomia',
+    'O Sol é uma estrela localizada no centro do Sistema Solar',
+    'O que é o Sol?'
+)
